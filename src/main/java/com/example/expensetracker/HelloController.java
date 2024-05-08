9b315d15512774c17fcd0a917cc808ad1bae9ff0
Abhinav Pareek <2022btcse002@curaj.ac.in>
2024-05-08 11:33:03 +0530

    adding sign up button

--- src/main/java/com/example/expensetracker/HelloController.java
+++ src/main/java/com/example/expensetracker/HelloController.java
@@ -1,7 +1,6 @@
 package com.example.expensetracker;
 
 import javafx.event.ActionEvent;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -11,7 +10,6 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.*;
-import java.util.*;
 
 class User implements Serializable {
     private String username;
@@ -42,11 +40,19 @@ class User implements Serializable {
 public class HelloController {
     private Stage stage;
     @FXML
+    TextField emailFieldSU;
+    @FXML
     TextField emailField;
     @FXML
     TextField passwordField;
     @FXML
     Label errorLabel;
+    @FXML
+    Label errorLabelSU;
+    @FXML
+    TextField passwordFieldSU;
+    @FXML
+    TextField confirmPasswordFieldSU;
     private String getUsername(String email){
         return email.substring(0, email.indexOf('@'));
     }
@@ -82,25 +88,117 @@ public class HelloController {
     }
 
     public void onLoginButton(ActionEvent actionEvent){
-        try {
-            FileInputStream userFile = login(emailField.getText(), passwordField.getText());
-            if (userFile != null) {
-                try {
-                    Parent root= FXMLLoader.load(getClass().getResource("MainPage.fxml"));
-                    stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
-                    Scene scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.setTitle("Expanse Tracker");
-                    stage.show();
+        if(!emailField.getText().isEmpty()) {
+            String email = emailField.getText();
+            if (!email.contains("@"))
+                errorLabel.setText("Invalid email!");
+            String password = passwordField.getText();
+            try {
+                FileInputStream userFile = login(email, password);
+                if (userFile != null) {
+                    try {
+                        Parent root = FXMLLoader.load(getClass().getResource("MainPage.fxml"));
+                        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.setTitle("Expanse Tracker");
+                        stage.show();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    errorLabel.setText("Invalid email or password!");
                 }
-                catch (Exception e){
-                    e.printStackTrace();
-                }
-            } else {
+            } catch (FileNotFoundException e) {
                 errorLabel.setText("Invalid email or password!");
+                System.out.println(e);
+            } catch (Exception e) {
+                errorLabel.setText("Invalid email or password!");
+                System.out.println(e);
             }
-        } catch (FileNotFoundException e) {
+        }
+    }
+    public void onRegisterButton(ActionEvent actionEvent){
+        try {
+            Parent root= FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
+            stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Expanse Tracker");
+            stage.show();
+        }
+        catch (Exception e){
             e.printStackTrace();
+        }
+    }
+    public void onRegisterToLogin(ActionEvent actionEvent){
+        try {
+            Parent root= FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+            stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Expanse Tracker");
+            stage.show();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public boolean isUserExist(String username){
+        try {
+            FileInputStream fileIn = new FileInputStream("admin.txt");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            while (true) {
+                User user = (User) in.readObject();
+                if (user.getUsername().equals(username)) {
+                    return true;
+                }
+            }
+        } catch (EOFException e) {
+            // End of file reached
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+    public void onSignUpButton(ActionEvent actionEvent){
+        String email = emailFieldSU.getText();
+        if(!email.contains("@"))
+            errorLabelSU.setText("Invalid email!");
+        String username = getUsername(email);
+        if(isUserExist(username)){
+            errorLabelSU.setText("User already exists!");
+        }else{
+            if(passwordFieldSU.getText().length()<8){
+                errorLabelSU.setText("Password must be at least 8 characters long!");
+            }
+            else {
+                if (passwordFieldSU.getText().equals(confirmPasswordFieldSU.getText())) {
+                    User user = new User(username, passwordFieldSU.getText(), 0);
+                    try {
+                        FileOutputStream fileOut = new FileOutputStream("admin.txt");
+                        ObjectOutputStream out = new ObjectOutputStream(fileOut);
+                        out.writeObject(user);
+                        out.close();
+                        fileOut.close();
+                        errorLabel.setText("User registered successfully!");
+
+                        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+                        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                        Scene scene = new Scene(root);
+                        stage.setScene(scene);
+                        stage.setTitle("Expanse Tracker");
+                        stage.show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    catch (Exception e){
+                        e.printStackTrace();
+                    }
+                } else {
+                    errorLabelSU.setText("Passwords do not match!");
+                }
+            }
         }
     }
 }
