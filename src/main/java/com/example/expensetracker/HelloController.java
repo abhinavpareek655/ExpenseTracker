7258d08cd3981a145ea536f36e35fba13400055a
Abhinav Pareek <2022btcse002@curaj.ac.in>
2024-05-08 16:06:24 +0530

    adding email varification system

--- src/main/java/com/example/expensetracker/HelloController.java
+++ src/main/java/com/example/expensetracker/HelloController.java
@@ -1,15 +1,26 @@
 package com.example.expensetracker;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import java.io.*;
+import java.net.URL;
+import java.util.Properties;
+import java.util.Random;
+import java.util.ResourceBundle;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
 
 class User implements Serializable {
     private String username;
@@ -48,8 +59,11 @@ class AppendableObjectOutputStream extends ObjectOutputStream {
         reset();
     }
 }
-public class HelloController {
+public class HelloController{
+    private static User user;
     private Stage stage;
+    @FXML
+    TextField OTPField;
     @FXML
     TextField emailFieldSU;
     @FXML
@@ -64,7 +78,49 @@ public class HelloController {
     TextField passwordFieldSU;
     @FXML
     TextField confirmPasswordFieldSU;
-    private String getUsername(String email){
+    private static String otp;
+    public String generateOTP(){
+        int[][] randomRanges = {{48,57},{65,90},{97,122}};
+        Random random = new Random();
+        StringBuilder otp = new StringBuilder();
+        for (int i=0;i<5;i++){
+            int dice = random.nextInt(0,randomRanges.length);
+            otp.append((char)random.nextInt(randomRanges[dice][0],randomRanges[dice][1]));
+        }
+        return otp.toString();
+    }
+    public void sendEmail(String message, String subject, String to, String from){
+        String host = "smtp.gmail.com";
+        Properties properties = System.getProperties();
+        System.out.println("PROPERTIES: "+properties);
+        properties.put("mail.smtp.host",host);
+        properties.put("mail.smtp.port","465");
+        properties.put("mail.smtp.ssl.enable","true");
+        properties.put("mail.smtp.auth","true");
+
+        Session session = Session.getInstance(properties, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("2022btcse002@curaj.ac.in","#$1000000");
+            }
+        });
+
+        session.setDebug(true);
+
+        MimeMessage m = new MimeMessage(session);
+        try {
+            m.setFrom(from);
+            m.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            m.setSubject(subject);
+            m.setText(message);
+            Transport.send(m);
+            System.out.println("Sent!!");
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    private static String getUsername(String email){
         return email.substring(0, email.indexOf('@'));
     }
     private String getPassword(String username){
@@ -199,24 +255,27 @@ public class HelloController {
             errorLabelSU.setText("User already exists!");
         }
         else{
-            errorLabelSU.setText("");
             if(passwordFieldSU.getText().length()<8){
                 errorLabelSU.setText("Password must be at least 8 characters long!");
             }
             else {
                 if (passwordFieldSU.getText().equals(confirmPasswordFieldSU.getText())) {
                     username = getUsername(emailFieldSU.getText());
-                    User user = new User(username, passwordFieldSU.getText(),Integer.MAX_VALUE);
+                    user = new User(username, passwordFieldSU.getText(),Integer.MAX_VALUE);
                     try {
-                        boolean append = new File("admin.txt").length() > 0;
-                        FileOutputStream fileOut = new FileOutputStream(new File("admin.txt"), true);
-                        ObjectOutputStream out = append ? new AppendableObjectOutputStream(fileOut) : new ObjectOutputStream(fileOut);
-                        out.writeObject(user);
-                        out.close();
-                        fileOut.close();
-                        errorLabelSU.setText("User registered successfully!");
-                        Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
-                        stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+                        otp = generateOTP();
+                        new Thread(() -> {
+                            sendEmail(otp,"OTP for Expanse Tracker",email,"2022btcse002@curaj.ac.in");
+                        }).start();
+////                        boolean append = new File("admin.txt").length() > 0;
+////                        FileOutputStream fileOut = new FileOutputStream(new File("admin.txt"), true);
+////                        ObjectOutputStream out = append ? new AppendableObjectOutputStream(fileOut) : new ObjectOutputStream(fileOut);
+////                        out.writeObject(user);
+////                        out.close();
+////                        fileOut.close();
+////                        errorLabelSU.setText("User registered successfully!");
+                        Parent root= FXMLLoader.load(getClass().getResource("OTPPage.fxml"));
+                        stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
                         Scene scene = new Scene(root);
                         stage.setScene(scene);
                         stage.setTitle("Expanse Tracker");
@@ -230,6 +289,41 @@ public class HelloController {
                 } else {
                     errorLabelSU.setText("Passwords do not match!");
                 }
+            }
+        }
+    }
+    public void onChangeEmailButton(ActionEvent actionEvent){
+        try{
+            Parent root= FXMLLoader.load(getClass().getResource("SignUpPage.fxml"));
+            stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("Expanse Tracker");
+            stage.show();
+        }
+        catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public void onVerifyButton(ActionEvent actionEvent){
+        if(OTPField.getText().equals(otp)){
+//            System.out.println("OTP verified");
+            try{
+                boolean append = new File("admin.txt").length() > 0;
+                FileOutputStream fileOut = new FileOutputStream(new File("admin.txt"), true);
+                ObjectOutputStream out = append ? new AppendableObjectOutputStream(fileOut) : new ObjectOutputStream(fileOut);
+                out.writeObject(user);
+                out.close();
+                fileOut.close();
+                Parent root = FXMLLoader.load(getClass().getResource("LoginPage.fxml"));
+                stage = (Stage)((Node)actionEvent.getSource()).getScene().getWindow();
+                Scene scene = new Scene(root);
+                stage.setScene(scene);
+                stage.setTitle("Expanse Tracker");
+                stage.show();
+            }
+            catch (Exception e){
+                System.out.println(e);
             }
         }
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.expensetracker {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.mail;
 
 
     opens com.example.expensetracker to javafx.fxml;
